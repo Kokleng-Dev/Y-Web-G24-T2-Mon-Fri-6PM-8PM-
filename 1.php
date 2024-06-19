0e40bb1c235b4905d7b7946b32f4b9e89d2cc609
Kokleng-Dev <y.koklengmedd@gmail.com>
2024-06-19 19:56:04 +0700

    push

--- 1.php
+++ 1.php
@@ -1,13 +1,5 @@
 <?php
-echo "hello 1 php blah blah 123";
-echo "this is my code from developer 1";
-echo "hello from developer 2";
-echo "hehe my thing ....";
-echo "hello php from dev 1";
-echo "123";
-echo "c++";
-echo "hehe my thing ....";
-echo "123";
+echo "hello kokleng";
 // local variable
 // $b = 'test';
 // function name(){
