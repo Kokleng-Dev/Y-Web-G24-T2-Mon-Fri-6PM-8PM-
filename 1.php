4cc4eb4a02787a8bfd7d7f3635deae25d0597c7d
Kokleng-Dev <y.koklengmedd@gmail.com>
2024-06-19 19:31:27 +0700

    new code

--- 1.php
+++ 1.php
@@ -3,6 +3,7 @@ echo "hello 1 php blah blah 123";
 echo "this is my code from developer 1";
 echo "hello from developer 2";
 echo "hehe my thing ....";
+echo "hello php from dev 1";
 // local variable
 // $b = 'test';
 // function name(){
